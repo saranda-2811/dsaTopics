7a49e4b038c3d776ee9f477f5e35f3905bfb7deb
Saranda Poddar <55356439+saranda-2811@users.noreply.github.com>
2022-06-06 12:18:33 +0530

    Create postOrder.cpp

---
+++ Tree/postOrder.cpp
@@ -0,0 +1,32 @@
+//Iterative post order traversal in binary tree
+
+// HINT: use two stacks
+
+vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL)
+            return ans;
+  
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+  
+        while(!st1.empty()) 
+        {
+            TreeNode* node = st1.top();
+            st1.pop();
+            st2.push(node);
+          
+            if(node->left!=NULL)
+                st1.push(node->left);
+            if(node->right!=NULL)
+                st1.push(node->right);
+        }
+  
+        while(!st2.empty())
+        {
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+  
+        return ans;
+    }
