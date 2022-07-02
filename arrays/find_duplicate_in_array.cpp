376f347c565fa54d85614c02b8ec202f7d62f821
Saranda Poddar <55356439+saranda-2811@users.noreply.github.com>
2022-07-02 15:45:40 +0530

    Create find_duplicate_in_array.cpp

---
+++ arrays/find_duplicate_in_array.cpp
@@ -0,0 +1,21 @@
+//  https://leetcode.com/problems/find-the-duplicate-number/
+
+int findDuplicate(vector<int>& nums) {
+        //cycling method
+        
+        int slow=nums[0], fast=nums[0];
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        
+        fast = nums[0];
+        
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
