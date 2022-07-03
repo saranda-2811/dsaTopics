84345c3fa4b6458e4e06c108e66135079c72420d
Saranda Poddar <55356439+saranda-2811@users.noreply.github.com>
2022-07-03 12:01:13 +0530

    Create power_x_raised_n.cpp

---
+++ arrays/power_x_raised_n.cpp
@@ -0,0 +1,27 @@
+//  https://leetcode.com/problems/powx-n/
+
+//  TC = O(log n)
+double myPow(double x, int n) {
+        double ans=1.0;
+        long long nn=n;
+        if(nn<0)
+            nn=-1*nn;
+        
+        while(nn>0)
+        {
+            if(nn%2==1)
+            {
+                ans = ans * x;
+                nn = nn-1;
+            }
+            else
+            {
+                x = x*x;
+                nn/=2;
+            }
+        }
+        
+        if(n<0)
+            ans = (double)(1/ans);
+        return ans;
+    }
